64f263ac7d27474b180c430e13922e5b227ab39c
andy12530 <andy12530@gmail.com>
2012-11-11 13:14:38 +0800

    修正若干bug，增加新组件支持 Signed-off-by: andy12530 <andy12530@gmail.com>

--- metrocss.js
+++ metrocss.js
@@ -1,4 +1,9 @@
 ﻿$(function() {
+	//prettyPrint();
+	if (window.prettyPrint) {
+		window.prettyPrint();
+	}	
+
 	// placeholder function
 	~function() {
 		function placeholder() {
@@ -22,6 +27,30 @@
 		}
 	}();
 
+	$("input[data-content]").bind("focus", function() {
+		$(this).popover("show");
+	}).bind("blur", function() {
+		$(this).popover("hide");
+	})
+
+	//dropselect
+	~function() {
+		var hideMenu;
+		$(".dropselect-ok, .dropselect-menu").bind("mouseenter", function() {
+			clearTimeout(hideMenu);
+			$(".dropselect-menu").slideDown(200);
+		}).bind("mouseleave", function() {
+			hideMenu = setTimeout(function() {
+				$(".dropselect-menu").slideUp(80);
+			}, 400);			
+		});
+
+		$(".dropselect-menu").on("click", "a", function(e) {
+			e.preventDefault();
+			$(".dropselect-ok a").text($(this).text());
+			$(".dropselect-menu").slideUp(80);
+		})
+	}()
 	// close alert
 	var Alert = function() {}
 
@@ -67,21 +96,21 @@
 
 	// popover
 	var Popover = function(elem, option) {
-			var placement = option.placement || "left",
+			var placement = option.placement || elem.data("placement") || "left",
 				title = option.title || elem.data("title") || "",
 				content = option.content || elem.data("content"),
 				className = option.className || "",
 				offset = elem.offset()
+				css = {};
 				if(placement == "left") {
 					css = {
-						'left': offset.left + elem.outerWidth() + 15,
+						'left': offset.left + elem.outerWidth() + 10,
 						'top': offset.top
 					}
 				} else {
 					css = {
 						'left': offset.left,
-						'top': offset.top + elem.outerHeight() + 15
-
+						'top': offset.top + elem.outerHeight() + 10
 					}
 				}
 			var titleHmtl = ""
@@ -91,13 +120,11 @@
 			html = '<div class="popover ' + className + '" style="z-index:1000; display:none; position: absolute;"><span class="popover-pointer ' + placement + '"><s></s></span>' + titleHmtl + '<div class="popover-content"><p>' + content + '</p></div></div>';
 
 			elem.after($(html).css(css));
-
 			this.elem = elem;
 		}
 
 	Popover.prototype = {
 		Constructor: Popover,
-
 		show: function() {
 			this.elem.next().show();
 		},
@@ -126,6 +153,67 @@
 		});
 	}
 
+	//弹出层
+	var Modal = function(elem, option) {
+		this.elem = elem;
+
+		if (option && option.keyboard === false) {
+			return false;
+		} else {
+			$("body").bind("keyup", function(e) {
+				if (e.which === 27) {
+					$(".modal").modal("hide");
+				}		
+			})
+		}
+	};
+
+	Modal.prototype = {
+		Contructor: Modal,
+
+		show: function() {
+			var elem = this.elem,
+				left = (document.documentElement.clientWidth - elem.outerWidth())/2,
+				top = (document.documentElement.clientHeight - elem.outerHeight())/2 ;
+
+			$("body").append('<div class="modal-drop"></div>');
+			elem.css({'left': left, 'top': top}).slideDown(200, function() {
+				$(this).trigger("shown");
+			});
+		},
+
+		hide: function() {
+			$(".modal-drop").remove();
+			this.elem.slideUp(100, function() {
+				$(this).trigger("hiden");
+			});
+		}
+	}
+
+	$.fn.modal = function(option) {
+		return $(this).each(function() {
+			$this = $(this);
+			if(!$this.data("modal")) {
+				$this.data("modal", new Modal($this, option));
+			}
+			if(typeof option == 'string') {
+				$this.data("modal")[option]();
+			}
+		})
+	}
+
+	~function() {
+		$(".modal-close").bind("click", function(e) {
+			e.preventDefault();
+			$(this).parents(".modal").modal("hide");
+		})
+
+		$("[data-target]").bind("click", function() {
+			var target = $(this).data("target");
+			$(target).modal("show");
+		})
+	}()
+
 	//按钮组
 	$(".btn-group").on("click", ".btn", function(e) {
 		e.preventDefault();
@@ -138,5 +226,4 @@
 		}
 		$this.toggleClass("active");
 	})
-
 });
